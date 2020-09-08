491222bcca59e73cb66abd35cf453bbf77a9d4da
Prokhor08 <70946383+Prokhor08@users.noreply.github.com>
2020-09-08 12:45:36 +0300

    Create timetable.py

---
+++ timetable.py
@@ -0,0 +1,23 @@
+def day_update(day):
+    return (day + 1) % 6
+
+timetable = [
+    [
+        'lesson1'
+    ], 
+    [
+        'lesson2'
+    ],
+    [
+        'lesson3'
+    ],
+    [
+        'lesson4'
+    ],
+    [
+        'lesson5'
+    ],
+    [
+        'lesson6'
+    ]
+]
