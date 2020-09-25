b25d074ababe2c001267ecda44006f84be391e66
fox <ivan51673@gmail.com>
2020-09-25 16:48:28 +0300

    Fix

--- main.py
+++ main.py
@@ -1,58 +1,70 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram import types
-from Keyboard import keyboard, lessons_num, removeKeyboard, keyboards
-from config import API_TOKEN, API_TOKEN2
-from timetable import timetable, day_update
-from datetime import datetime
-from aiogram.utils.markdown import text, bold, italic, code, pre
-from collections import deque
-import pytz
 import asyncio
+from collections import deque
+from datetime import datetime
 
-#utils
+import pytz
+from aiogram import Bot, Dispatcher, executor
+from aiogram import types
+from config import API_TOKEN, API_TOKEN2
+
+from Keyboard import keyboard, lessons_num, keyboards
+from timetable import timetable
+
+
+# utils
 async def send_msg(user_id, text):
     await bot.send_message(chat_id=user_id, text=text)
+
+
 def now():
     noww = datetime.now(pytz.timezone('Europe/Moscow'))
     return noww
-def text_on_button(keyboard_name, button_num):
 
+
+def text_on_button(keyboard_name, button_num):
     curnum = -1
-    text = ''   
+    text = ''
     for row in keyboards[keyboard_name]:
         for button in row:
             curnum += 1
-            if(curnum == button_num):
+            if (curnum == button_num):
                 text = button['text']
 
     return text
+
+
 def text_in_msg(keyboard_name, button_num):
     text = text_on_button(keyboard_name, button_num)
     return lambda message: message.text and text in message.text
+
+
 def text_is_lesson_name():
     def check(text):
         for num in range(lessons_num):
             if text_on_button('lessons', num) == text:
                 return True
         return False
-    
+
     return lambda message: message.text and check(message.text)
+
 
 UPDATE_INTERVAL = 300
 bot = Bot(token=API_TOKEN, parse_mode="HTML")
 disp = Dispatcher(bot)
 
-inf_bot = Bot(token = API_TOKEN2)
+inf_bot = Bot(token=API_TOKEN2)
 inf_disp = Dispatcher(bot)
 
 state = {}
 updates_by_user = {}
 last_keyboard = {}
 
-day = now().weekday()%6
+day = now().weekday() % 6
 
 admins_id = [800155626]
-users_ids = [800155626, 664331079, 998445492, 912050293, 652242346, 723471766, 539584923, 1249475977, 918018751, 939427187, 1035364674, 871823293, 792033308, 604117040, 892138456, 902858644 ]
+users_ids = [800155626, 664331079, 998445492, 912050293, 652242346, 723471766, 539584923, 1249475977, 918018751,
+             939427187, 1035364674, 871823293, 792033308, 604117040, 892138456, 902858644]
+
 
 class Strings:
     def __init__(self):
@@ -77,6 +89,7 @@ class Strings:
         self.bad = "BAD!"
         self.incorrect_task = "Некорректное задание!"
 
+
 strings = Strings()
 
 
@@ -85,15 +98,15 @@ class Hometask:
         self.hometask = {}
         self.docs = {}
         self.photos = {}
-    
+
     def update_task(self, subject_name, task):
         self.hometask[subject_name] = task
-    
+
     def update_files(self, subject_name, files):
         self.docs[subject_name] = []
         for file in files:
             self.docs[subject_name].append(file)
-    
+
     def update_photos(self, subject_name, files):
         self.photos[subject_name] = []
         for file in files:
@@ -101,10 +114,10 @@ class Hometask:
 
     def clear_task(self, subject_name):
         self.hometask[subject_name] = None
-    
+
     def clear_docs(self, subject_name):
         self.docs[subject_name] = []
-    
+
     def clear_photos(self, subject_name):
         self.photos[subject_name] = []
 
@@ -112,7 +125,7 @@ class Hometask:
         if (not subject_name in self.hometask) or (self.hometask[subject_name] == None):
             return strings.no_task
         return self.hometask[subject_name]
-    
+
     def get_files(self, subject_name):
         if (not subject_name in self.docs) or (self.docs[subject_name] == None):
             return []
@@ -140,21 +153,22 @@ class Hometask:
                 task = None
             else:
                 task = self.hometask[subject_name]
-            
+
             if task == None:
                 hometask += strings.tab + strings.no_task
             else:
                 hometask += task
-            
+
             if self.photos[subject_name] != [] or self.docs[subject_name] != []:
                 hometask += strings.tab + strings.have_files + "\n"
-        
+
         return hometask
 
 
 hometask = Hometask()
 
-#admin tools
+
+# admin tools
 async def change_timetable(message: types.Message):
     if not message.from_user.id in admins_id:
         await message.reply(strings.not_adm)
@@ -178,26 +192,28 @@ async def change_timetable(message: types.Message):
     for lesson in lessons:
         timetable[day_num].append(lesson)
 
-    await message.reply(text = strings.succesfully_updated)
+    await message.reply(text=strings.succesfully_updated)
+
 
 async def add_user(message: types.Message):
     uid = message.from_user.id
 
     if not uid in admins_id:
-        await bot.send_message(text = strings.no_adm)
+        await bot.send_message(text=strings.no_adm)
         return
-    
+
     text = message.text
-    text = text[len('/add_user ')  : len(text)]
+    text = text[len('/add_user '): len(text)]
 
     try:
         add_id = int(text)
         users_ids.append(add_id)
-        await message.reply(text = strings.succesfully_updated)
+        await message.reply(text=strings.succesfully_updated)
     except:
-        await message.reply(text = strings.incorrect_id)
+        await message.reply(text=strings.incorrect_id)
 
-async def del_user(message:types.Message):
+
+async def del_user(message: types.Message):
     uid = message.from_user.id
 
     if not uid in admins_id:
@@ -213,6 +229,7 @@ async def del_user(message:types.Message):
         await message.reply(text=strings.succesfully_cleared)
     except ValueError:
         await message.reply(text=strings.incorrect_id)
+
 
 async def add_admin(message: types.Message):
     uid = message.from_user.id
@@ -231,13 +248,14 @@ async def add_admin(message: types.Message):
     except:
         await message.reply(text=strings.incorrect_id)
 
+
 async def clear_hometask(message):
     uid = message.from_user.id
 
     if not uid in admins_id:
         message.reply(strings.no_adm)
-        return 
-    
+        return
+
     text = message.text
     text = text[len('/clear_hometask '):len(text)]
 
@@ -251,7 +269,8 @@ async def clear_hometask(message):
 
     await message.reply(strings.succesfully_cleared)
 
-#for all users
+
+# for all users
 async def start(message: types.Message):
     uid = message.from_user.id
 
@@ -261,9 +280,9 @@ async def start(message: types.Message):
 
     last_keyboard[uid] = deque()
     state[uid] = 0
-    updates_by_user[uid] = {'subject_name':"", 'text':"", 'files':[], 'photos':[]}
+    updates_by_user[uid] = {'subject_name': "", 'text': "", 'files': [], 'photos': []}
     last_keyboard[uid].append('main')
-    await message.reply(text = strings.start_message, reply_markup=keyboard('main'))
+    await message.reply(text=strings.start_message, reply_markup=keyboard('main'))
 
 
 async def get_hometask(message: types.Message):
@@ -274,14 +293,14 @@ async def get_hometask(message: types.Message):
         return
 
     if not uid in state:
-        await message.reply(text = strings.send_start)
+        await message.reply(text=strings.send_start)
         return
 
     if state[uid] == 3:
-        await message.reply(text = strings.bad_task)
+        await message.reply(text=strings.bad_task)
         return
 
-    await send_msg(user_id = uid, text = hometask.get_hometask())
+    await send_msg(user_id=uid, text=hometask.get_hometask())
 
 
 async def send_lessons(message: types.Message, typ):
@@ -294,15 +313,15 @@ async def send_lessons(message: types.Message, typ):
     if not uid in state:
         await message.reply(text=strings.send_start)
         return
-    
+
     if state[uid] == 3:
-        await message.reply(text = strings.bad_task)
+        await message.reply(text=strings.bad_task)
         return
 
     state[uid] = typ
 
     last_keyboard[uid].append('lessons')
-    await message.reply(text = strings.choose_subject, reply_markup=keyboard('lessons'))
+    await message.reply(text=strings.choose_subject, reply_markup=keyboard('lessons'))
 
 
 async def lesson_name_sended(message: types.Message):
@@ -315,25 +334,25 @@ async def lesson_name_sended(message: types.Message):
     if not uid in state:
         await message.reply(text=strings.send_start)
         return
-    
+
     if state[uid] == 1:
         await message.reply(text=hometask.get_task(message.text))
         for file in hometask.get_files(message.text):
-            await bot.send_document(chat_id = uid, document = file)
+            await bot.send_document(chat_id=uid, document=file)
         for file in hometask.get_photos(message.text):
-            await bot.send_photo(chat_id = uid, photo = file)
+            await bot.send_photo(chat_id=uid, photo=file)
     elif state[uid] == 2:
         state[uid] = 3
         updates_by_user[uid]['subject_name'] = message.text
-        await message.reply(text=strings.send_task, reply_markup= keyboard('done'))
+        await message.reply(text=strings.send_task, reply_markup=keyboard('done'))
     elif state[uid] == 3:
-        await message.reply(text = strings.incorrect_task)
+        await message.reply(text=strings.incorrect_task)
         return
     else:
-        await message.reply(text = strings.use_buttons)
+        await message.reply(text=strings.use_buttons)
 
 
-async def others(message: types.Message):    
+async def others(message: types.Message):
     uid = message.from_user.id
 
     if not uid in users_ids:
@@ -343,10 +362,10 @@ async def others(message: types.Message):
     if not uid in state:
         await message.reply(text=strings.send_start)
         return
-    
+
     no_intresting_states = [0, 1, 2]
     if state[uid] in no_intresting_states:
-        await message.reply(text = strings.use_buttons)
+        await message.reply(text=strings.use_buttons)
     else:
         if state[uid] == 3:
 
@@ -362,7 +381,7 @@ async def others(message: types.Message):
             log_str += '(' + str(uid) + '): '
             log_str += message.text + '|' + updates_by_user[uid]['subject_name'] + "\n"
 
-            await inf_bot.send_message(chat_id = admins_id[0], text = log_str)
+            await inf_bot.send_message(chat_id=admins_id[0], text=log_str)
 
             updates_by_user[uid]['text'] += strings.tab + (str(message.text)) + "\n"
 
@@ -402,11 +421,11 @@ async def docs_handler(message: types.Message):
     if not uid in state:
         await message.reply(strings.send_start)
         return
-    
+
     if state[uid] != 3:
         await message.reply(strings.use_buttons)
         return
-    
+
     doc_id = message.document.file_id
 
     updates_by_user[uid]['files'].append(doc_id)
@@ -422,7 +441,7 @@ async def docs_handler(message: types.Message):
     log_str += ')'
     log_str += '(' + str(uid) + ') отправил фото|' + updates_by_user[uid]['subject_name'] + "\n"
 
-    await inf_bot.send_message(chat_id = uid, text = log_str)
+    await inf_bot.send_message(chat_id=uid, text=log_str)
 
 
 async def photo_handler(message: types.Message):
@@ -455,8 +474,8 @@ async def photo_handler(message: types.Message):
     log_str += ')'
     log_str += '(' + str(uid) + ') отправил фото|' + updates_by_user[uid]['subject_name'] + "\n"
 
-    await inf_bot.send_message(chat_id = uid, text = log_str)
-    
+    await inf_bot.send_message(chat_id=uid, text=log_str)
+
 
 async def done(message: types.Message):
     uid = message.from_user.id
@@ -468,21 +487,21 @@ async def done(message: types.Message):
     if not uid in state:
         await message.reply(strings.send_start)
         return
-    
+
     if state[uid] != 3:
         await message.reply(strings.use_buttons)
         return
-    
+
     text = updates_by_user[uid]['text']
 
     if len(text) != 0:
         hometask.update_task(updates_by_user[uid]['subject_name'], text)
 
-    files = updates_by_user[uid]['files']     
+    files = updates_by_user[uid]['files']
     if len(files) != 0:
         hometask.update_files(updates_by_user[uid]['subject_name'], files)
-    
-    photos = updates_by_user[uid]['photos'] 
+
+    photos = updates_by_user[uid]['photos']
     if len(photos) != 0:
         hometask.update_photos(updates_by_user[uid]['subject_name'], photos)
 
@@ -491,7 +510,8 @@ async def done(message: types.Message):
     updates_by_user[uid]['photos'] = []
     updates_by_user[uid]['text'] = ""
     state[uid] = 2
-    await bot.send_message(chat_id = uid, text = strings.done, reply_markup=keyboard('lessons'))
+    await bot.send_message(chat_id=uid, text=strings.done, reply_markup=keyboard('lessons'))
+
 
 async def updating():
     global day
@@ -509,46 +529,44 @@ async def updating():
             day %= 6
 
             await inf_bot.send_message(chat_id=admins_id[0], text="DAY UPDATED!")
-        
+
         if now().hour == 13:
             was_update = False
-        
+
         await inf_bot.send_message(admins_id[0], text="Updating...")
 
         await asyncio.sleep(UPDATE_INTERVAL)
 
 
 def Bot():
-
-    @disp.message_handler(commands = ['add_user'])
+    @disp.message_handler(commands=['add_user'])
     async def add_userr(message: types.Message):
         add_user(message)
 
-    
-    @disp.message_handler(commands = ['add_admin'])
+    @disp.message_handler(commands=['add_admin'])
     async def add_adminn(message: types.Message):
         add_admin(message)
 
-    @disp.message_handler(commands = ['change_timetable'])
+    @disp.message_handler(commands=['change_timetable'])
     async def change_timetablee(message: types.Message):
         await change_timetable(message)
-    
-    @disp.message_handler(commands = ['clear_hometask'])
+
+    @disp.message_handler(commands=['clear_hometask'])
     async def change_timetablee(message: types.Message):
         await clear_hometask(message)
 
     @disp.message_handler(commands=['start'])
     async def startt(message: types.Message):
         await start(message)
-    
+
     @disp.message_handler(text_in_msg('main', 0))
     async def get_hometaskk(message: types.Message):
         await get_hometask(message)
-    
-    @disp.message_handler(text_in_msg('main', 1) )
+
+    @disp.message_handler(text_in_msg('main', 1))
     async def send_lessonss(message: types.Message):
         await send_lessons(message, 1)
-    
+
     @disp.message_handler(text_in_msg('main', 2))
     async def send_lessonss2(message: types.Message):
         await send_lessons(message, 2)
@@ -560,7 +578,7 @@ def Bot():
     @disp.message_handler(text_is_lesson_name())
     async def lesson_namee(message: types.Message):
         await lesson_name_sended(message)
-    
+
     @disp.message_handler(text_in_msg('lessons', 13))
     async def backk(message: types.Message):
         await back(message)
@@ -569,7 +587,7 @@ def Bot():
     async def photo_handlerr(message: types.Message):
         await photo_handler(message)
 
-    @disp.message_handler(content_types = ['document'])
+    @disp.message_handler(content_types=['document'])
     async def docs_handlerr(message: types.Message):
         await docs_handler(message)
 
@@ -577,8 +595,7 @@ def Bot():
     async def otherss(message: types.Message):
         await others(message)
 
-    
-    disp.loop.create_task(updating())
+    (disp.loop or asyncio.get_event_loop()).create_task(updating())
     executor.start_polling(disp)
 
 
